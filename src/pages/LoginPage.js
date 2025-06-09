21e91de1021efba6de261bbdc471dd98f9f64317
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-06-09 10:48:23 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -17,7 +17,7 @@ const LoginPage = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { username, password });
+      const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/auth/login`, { username, password });
       localStorage.setItem('token', res.data.token);
       navigate('/');
     } catch (err) {
